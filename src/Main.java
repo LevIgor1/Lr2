66126b995bff3691081d214a34103bfd5cd3b103
sachko65 <levchencow@gmail.com>
2023-09-13 14:48:16 +0300

    Initial values 2 and working with them

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,11 @@ public class Main {
         float f1, f2;
         byte b = -10;
         boolean q = true;
+
+        s1 = (short)Math.pow(b, 2);
+        s2 = (short)(Math.random() * 10);
+        double d2 = Math.random() * (9999 - 1) + 1;
+        double d1 = d2 / s1 * s2;
+        System.out.println(d1);
     }
 }
